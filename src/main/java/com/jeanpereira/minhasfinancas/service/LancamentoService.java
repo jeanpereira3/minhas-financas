c833136817227e28ab5a8e1741324b385bc9f5e7
jeanpereira3 <jeansilv13@gmail.com>
2023-01-19 17:49:21 -0300

    adicionadas as classes LancamentoService e LancamentoServiceImpl

---
+++ src/main/java/com/jeanpereira/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,18 @@
+package com.jeanpereira.minhasfinancas.service;
+
+import java.util.List;
+
+import com.jeanpereira.minhasfinancas.model.entity.Lancamento;
+import com.jeanpereira.minhasfinancas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+	Lancamento salvar(Lancamento lancamento);
+	
+	Lancamento atualizar(Lancamento lancamento);
+	
+	void deletar(Lancamento lancamento);
+	
+	List<Lancamento> buscar(Lancamento lancamentoFiltro);
+	
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+}
---
+++ src/main/java/com/jeanpereira/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,41 @@
+package com.jeanpereira.minhasfinancas.service.impl;
+
+import java.util.List;
+
+import com.jeanpereira.minhasfinancas.model.entity.Lancamento;
+import com.jeanpereira.minhasfinancas.model.enums.StatusLancamento;
+import com.jeanpereira.minhasfinancas.service.LancamentoService;
+
+public class LancamentoServiceImpl implements LancamentoService {
+
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
