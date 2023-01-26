611810bd5d12a12fad8a55a070935c784cfc35b0
jeanpereira3 <jeansilv13@gmail.com>
2023-01-26 11:17:12 -0300

    adicionado construtor na classe UsuarioDTO

--- src/main/java/com/jeanpereira/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/jeanpereira/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,10 +1,14 @@
 package com.jeanpereira.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
 	private String nome;
