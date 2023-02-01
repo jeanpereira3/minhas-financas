e4d285a83d285eeda48aada9d184f6fb25914482
jeanpereira3 <jeansilv13@gmail.com>
2023-02-01 13:46:04 -0300

    agora apenas o saldo de lancamentos efetivados serao recuperados

--- src/main/java/com/jeanpereira/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/jeanpereira/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.jeanpereira.minhasfinancas.model.entity.Lancamento;
+import com.jeanpereira.minhasfinancas.model.enums.StatusLancamento;
 import com.jeanpereira.minhasfinancas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
@@ -15,10 +16,14 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 			value = 
 			" select sum(l.valor) "
 			+ "from Lancamento l join l.usuario u "
-			+ "where u.id = :idUsuario and l.tipo = :tipo "
+			+ "where "
+			+ "u.id = :idUsuario "
+			+ "and l.tipo = :tipo "
+			+ "and l.status = :status "
 			+ "group by u")
 	BigDecimal obterSaldoPorTipoLancamentoEUsuario(
 			@Param("idUsuario") Long idUsuario,
-			@Param("tipo") TipoLancamento tipo
+			@Param("tipo") TipoLancamento tipo,
+			@Param("status") StatusLancamento status
 	);
 }
--- src/main/java/com/jeanpereira/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/jeanpereira/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -107,9 +107,9 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Transactional(readOnly = true)
 	public BigDecimal obterSaldoPorUsuario(Long id) {
 		BigDecimal receitas = repository
-				.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
+				.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA, StatusLancamento.EFETIVADO);
 		BigDecimal despesas = repository
-				.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+				.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA, StatusLancamento.EFETIVADO);
 		
 		if (receitas == null) {
 			receitas = BigDecimal.ZERO;
